d018f1a5263955614c04766df3bafe9157b05017
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-04-24 17:32:55 +0200

    Added Github_desktop.sh script.

---
+++ Github_desktop.sh
@@ -0,0 +1,3 @@
+rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
+sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo'
+dnf install --assumeyes github-desktop
--- README.md
+++ README.md
@@ -9,3 +9,7 @@ A script that boost DNF and help enabling certain features of DNF.
 ## MS_Fonts_fedora
 
 A script that install the major Microsoft fonts such as Times New Roman or Arial.
+
+## Github_desktop
+
+A script that install the Github desktop software on Fedora.
