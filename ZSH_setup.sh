f5ef22827c827707188c0037ed610ea244c721d1
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-04-24 18:02:55 +0200

    Update ZSH_setup.sh

--- ZSH_setup.sh
+++ ZSH_setup.sh
@@ -7,4 +7,4 @@ sh install.sh
 
 sed -i 's/robbyrussell/bira/g' ~/.zshrc
 
-sed -i 's/plugins=()/plugins=(git dnf zsh-autosuggestions)/g' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git dnf zsh-autosuggestions)/g' ~/.zshrc
