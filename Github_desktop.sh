6036b83aa10882c1d6b41274e82d4f2f199f894c
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-04-24 18:06:41 +0200

    Update Github_desktop.sh

--- Github_desktop.sh
+++ Github_desktop.sh
@@ -1,3 +1,5 @@
+# /bin/sh
+
 rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
 sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo'
 dnf install --assumeyes github-desktop
