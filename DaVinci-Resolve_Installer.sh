f75db3a93e078c97c7d94fef1621189689074206
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-05-02 09:33:25 +0200

    Replace firefox with xdg-open.

--- DaVinci-Resolve_Installer.sh
+++ DaVinci-Resolve_Installer.sh
@@ -4,7 +4,7 @@ while true; do
     read -p "Do you want to be redirected to the website to download the ZIP ? [Y/n]" yn
     case $yn in
         [Yy]* ) break;;
-        [Nn]* ) firefox https://www.blackmagicdesign.com/support/download/f73a0db92053469f8b0032cf1f4bb220/Linux ; break;;
+        [Nn]* ) xdg-open https://www.blackmagicdesign.com/support/download/f73a0db92053469f8b0032cf1f4bb220/Linux ; break;;
         * ) break;;
     esac
 done
