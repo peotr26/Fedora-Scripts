41fcef7943baf5f390dba20fe1a301a08a7a8b3e
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-05-15 18:36:39 +0200

    Removed nvtop.1 from ressources.

--- Nvtop_fedora.sh
+++ Nvtop_fedora.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 wget https://raw.githubusercontent.com/peotr26/Fedora-Scripts/main/Ressources/Nvtop_fedora/nvtop
-wget https://raw.githubusercontent.com/peotr26/Fedora-Scripts/main/Ressources/Nvtop_fedora/nvtop.1
+wget https://raw.githubusercontent.com/Syllo/nvtop/master/manpage/nvtop.in
 
 sudo cp nvtop /usr/local/bin
-sudo cp nvtop.1 /usr/local/share/man/man1
+sudo cp nvtop.in /usr/local/share/man/man1
 
 sudo chmod +x /usr/local/bin/nvtop
+
+sudo mv /usr/local/share/man/man1/nvtop.in nvtop.1
