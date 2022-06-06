ca1ad0d67aa898423802e05510872f9fe619ce10
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-06-06 09:07:00 +0200

    Updated to the latest PWAForFirefox version.

--- PWA_Firefox.sh
+++ PWA_Firefox.sh
@@ -5,5 +5,5 @@ firefox -install-global-extension progressive_web_apps_for_firefox-fx.xpi
 
 Arch=`uname -m`
 
-wget 'https://github.com/filips123/PWAsForFirefox/releases/download/v1.4.0/firefoxpwa-1.4.0-1.'$Arch'.rpm'
-sudo dnf install --assumeyes 'firefoxpwa-1.4.0-1.'$Arch'.rpm'
+wget 'https://github.com/filips123/PWAsForFirefox/releases/download/v1.4.2/firefoxpwa-1.4.2-1.'$Arch'.rpm'
+sudo dnf install --assumeyes 'firefoxpwa-1.4.2-1.'$Arch'.rpm'
