a75c9622b5a416fbc2a28b0202e7c7136ba2e484
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-06-12 16:17:41 +0200

    Cleaned the script and updated from 3.0.0 to 3.0.1

--- Github_desktop.sh
+++ Github_desktop.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-#rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
-#sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo'
-#dnf install --assumeyes github-desktop
-sudo dnf install --assumeyes https://github.com/shiftkey/desktop/releases/download/release-3.0.0-linux1/GitHubDesktop-linux-3.0.0-linux1.rpm
+sudo dnf install --assumeyes https://github.com/shiftkey/desktop/releases/download/release-3.0.1-linux1/GitHubDesktop-linux-3.0.1-linux1.rpm
