9791076d303649b8a051f96300d40bdc03698cd0
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-08-25 18:22:35 +0200

    Update Nix_fedora.sh
    The script will now configure SELinux to be able to use the multi-user configuration.

--- Scripts/Nix_fedora.sh
+++ Scripts/Nix_fedora.sh
@@ -9,3 +9,12 @@ while true; do
         * ) break;;
     esac
 done
+
+sudo semanage fcontext -a -t etc_t '/nix/store/[^/]+/etc(/.*)?'
+sudo semanage fcontext -a -t lib_t '/nix/store/[^/]+/lib(/.*)?'
+sudo semanage fcontext -a -t systemd_unit_file_t '/nix/store/[^/]+/lib/systemd/system(/.*)?'
+sudo semanage fcontext -a -t man_t '/nix/store/[^/]+/man(/.*)?'
+sudo semanage fcontext -a -t bin_t '/nix/store/[^/]+/s?bin(/.*)?'
+sudo semanage fcontext -a -t usr_t '/nix/store/[^/]+/share(/.*)?'
+sudo semanage fcontext -a -t var_run_t '/nix/var/nix/daemon-socket(/.*)?'
+sudo semanage fcontext -a -t usr_t '/nix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
